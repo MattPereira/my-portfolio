8015b713def71952642ea1ccb50c4e8bec6c95eb
Matthew Pereira <matthewdavidpereira@gmail.com>
2023-12-13 16:30:53 -0800

    style breakthrough

--- packages/nextjs/components/portfolio/SectionContainer.tsx
+++ packages/nextjs/components/portfolio/SectionContainer.tsx
@@ -5,7 +5,7 @@ interface ISectionContainer {
 
 export function SectionContainer({ children, bgcolor }: ISectionContainer) {
   return (
-    <section className={`py-16 ${bgcolor}`}>
+    <section className={`py-16 ${bgcolor}  border-t border-base-content`}>
       <div className="container mx-auto px-5 md:px-0">{children}</div>
     </section>
   );
